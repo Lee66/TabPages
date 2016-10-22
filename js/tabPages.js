ff2bf8c15ce1ece9f07c9239c5b088fc66e3830e
robotlee <robotlee8@163.com>
2016-10-22 22:13:06 +0800

    tab标签页小例子

---
+++ js/tabPages.js
@@ -0,0 +1,28 @@
+function TabSwitch(id)
+{
+	var oDiv=document.getElementById(id);
+	this.aBtn=oDiv.getElementsByClassName('tabHeaderItem');
+	this.aDiv=oDiv.getElementsByClassName('tabPageItem');
+	var _this=this;
+	
+	for(var i=0;i<this.aBtn.length;i++)
+	{
+		this.aBtn[i].index=i;
+		this.aBtn[i].onclick=function ()
+		{
+			_this.tab(this);
+		};
+	}
+}
+
+TabSwitch.prototype.tab=function (oBtn)
+{
+
+	for( var i=0;i<this.aBtn.length;i++)
+	{
+		this.aBtn[i].className='tabHeaderItem';
+		this.aDiv[i].style.display='none';
+	}
+	oBtn.className='tabHeaderItem tabPageHeaderActive';
+	this.aDiv[oBtn.index].style.display='block';
+};
